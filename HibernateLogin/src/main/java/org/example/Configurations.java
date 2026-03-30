bedc1f6e30df7bc7f6b13070a8e282ce12b5fc18
admin <ilianagof@gmail.com>
2026-03-30 10:19:34 +0300

    Cleanup HibernateLogin before sync

--- HibernateLogin/src/main/java/org/example/Configurations.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
-
-@ComponentScan
-@PropertySource("classpath:resources.properties")
-public class Configurations {}
